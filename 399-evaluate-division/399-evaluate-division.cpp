51e01f705252e63ae92836c016c84bcf4c2977bf
Gaurav somani <40043226+gauravsomani336@users.noreply.github.com>
2022-05-02 11:15:20 +0530

    Time: 4 ms (51.71%), Space: 8.4 MB (52.66%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    typedef unordered_map<string, unordered_map<string,double> >Graph;
+    typedef unordered_map<string, unordered_map<string, double>>Graph;
     Graph graph;
-    
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         
         int n=queries.size();
-        vector<double>res(n, -1.0);
+        vector<double>res(n , -1.0);
         
         buildgraph(equations, values);
-        for(int i=0;i<queries.size();i++)
+        
+        for(int i=0;i<n;i++)
         {
-            unordered_map<string, bool>visited;
+            unordered_map< string, bool>visited;
             res[i]= dfs(queries[i][0], queries[i][1], visited);
         }
         
@@ -20,48 +20,43 @@ public:
     
     void buildgraph(vector<vector<string>>& equations, vector<double>& values)
     {
-        for(int i=0;i<equations.size();i++)
+        int n=equations.size();
+        for(int i=0;i<n;i++)
         {
-            graph[equations[i][0]].insert({equations[i][1],values[i]});
-            graph[equations[i][1]].insert({equations[i][0],(double)1/values[i]});
+            graph[equations[i][0]].insert({equations[i][1], (double)values[i]});
+            graph[equations[i][1]].insert({equations[i][0], (double)1/values[i]});
         }
     }
     
-    double dfs(string s, string d, unordered_map<string, bool>&visited)
+    double dfs(string s , string d, unordered_map<string , bool>&visited)
     {
         if(graph.find(s)==graph.end())
         {
             return -1.0;
         }
-         if(s.compare(d)==0)
+        
+        if(s.compare(d)==0)
         {
             return 1.0;
         }
         
-        if(graph[s].find(d)!=graph[s].end())
+        if(graph[s].find(d)!= graph[s].end())
         {
             return graph[s][d];
         }
         
-
-        
-       
-        
         visited[s]=true;
-        
         for(auto it: graph[s])
         {
             if(visited[it.first]==false)
             {
-                double ans= dfs(it.first, d, visited);
-                if(ans!= -1.0)
+                double ans=dfs(it.first, d, visited);
+                if(ans!=-1.0)
                 {
-                   return (double)ans * (it.second);
+                    return it.second * (double)ans;
                 }
             }
         }
-        
         return -1.0;
-        
     }
 };
