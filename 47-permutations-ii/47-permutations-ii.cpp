25f890135810edfbbc2c8e50f513b7bb83c2a831
Gaurav somani <40043226+gauravsomani336@users.noreply.github.com>
2022-05-11 07:53:22 +0530

    Time: 15 ms (47.89%), Space: 8.6 MB (80.95%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto it: nums)
+        {
+            mp[it]+=1;
+        }
+        
+        vector<int>perm;
+        vector<vector<int>>res;
+        sort(nums.begin(), nums.end());
+        backtrack2(nums, perm, res, mp);
+        return res;
+    }
+    
+//     void solve(vector<int>&nums, vector<int>&perm, vector<vector<int>>&res, unordered_map<int, int>&mp)
+//     {
+//         if(perm.size()==nums.size())
+//         {
+//             res.push_back(perm);
+//             return ;
+//         }
+        
+//         for(int i=0;i<nums.size(); i++)
+//         {
+//             if(mp[nums[i]]==0  || (i!=0 && nums[i]==nums[i-1]))
+//             {
+//                 continue;
+//             }
+            
+//             perm.push_back(nums[i]);
+//             mp[nums[i]]--;
+            
+//             solve(nums, perm, res, mp);
+            
+           
+//             mp[nums[i]]+;
+//              perm.pop_back();
+//         }
+        
+        
+        
+//     }
+    void backtrack2(vector<int>& nums, vector<int>& temp,
+                  vector<vector<int>>& res, unordered_map<int, int>& freq) {
+        if (temp.size() == nums.size()) {
+            res.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (freq[nums[i]] == 0 || (i != 0 && nums[i] == nums[i - 1])) continue;
+            temp.push_back(nums[i]);
+            freq[nums[i]]--;
+            backtrack2(nums, temp, res, freq);
+            freq[nums[i]]++;
+            temp.pop_back();
+        }
+    }
+};
