5d2fc7d62cba64e10c5015289c8e75239312ce63
Gaurav somani <40043226+gauravsomani336@users.noreply.github.com>
2022-05-11 07:54:12 +0530

    Time: 8 ms (74.87%), Space: 8.5 MB (89.24%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -10,51 +10,51 @@ public:
         vector<int>perm;
         vector<vector<int>>res;
         sort(nums.begin(), nums.end());
-        backtrack2(nums, perm, res, mp);
+        solve(nums, perm, res, mp);
         return res;
     }
     
-//     void solve(vector<int>&nums, vector<int>&perm, vector<vector<int>>&res, unordered_map<int, int>&mp)
-//     {
-//         if(perm.size()==nums.size())
-//         {
-//             res.push_back(perm);
-//             return ;
-//         }
+    void solve(vector<int>&nums, vector<int>&perm, vector<vector<int>>&res, unordered_map<int, int>&mp)
+    {
+        if(perm.size()==nums.size())
+        {
+            res.push_back(perm);
+            return ;
+        }
         
-//         for(int i=0;i<nums.size(); i++)
-//         {
-//             if(mp[nums[i]]==0  || (i!=0 && nums[i]==nums[i-1]))
-//             {
-//                 continue;
-//             }
+        for(int i=0;i<nums.size(); i++)
+        {
+            if(mp[nums[i]]==0  || (i!=0 && nums[i]==nums[i-1]))
+            {
+                continue;
+            }
             
-//             perm.push_back(nums[i]);
-//             mp[nums[i]]--;
+            perm.push_back(nums[i]);
+            mp[nums[i]]-=1;
             
-//             solve(nums, perm, res, mp);
+            solve(nums, perm, res, mp);
             
            
-//             mp[nums[i]]+;
-//              perm.pop_back();
-//         }
-        
-        
-        
-//     }
-    void backtrack2(vector<int>& nums, vector<int>& temp,
-                  vector<vector<int>>& res, unordered_map<int, int>& freq) {
-        if (temp.size() == nums.size()) {
-            res.push_back(temp);
-            return;
+            mp[nums[i]]+=1;
+             perm.pop_back();
         }
-        for (int i = 0; i < nums.size(); i++) {
-            if (freq[nums[i]] == 0 || (i != 0 && nums[i] == nums[i - 1])) continue;
-            temp.push_back(nums[i]);
-            freq[nums[i]]--;
-            backtrack2(nums, temp, res, freq);
-            freq[nums[i]]++;
-            temp.pop_back();
-        }
+        
+        
+        
     }
+    // void backtrack2(vector<int>& nums, vector<int>& temp,
+    //               vector<vector<int>>& res, unordered_map<int, int>& freq) {
+    //     if (temp.size() == nums.size()) {
+    //         res.push_back(temp);
+    //         return;
+    //     }
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         if (freq[nums[i]] == 0 || (i != 0 && nums[i] == nums[i - 1])) continue;
+    //         temp.push_back(nums[i]);
+    //         freq[nums[i]]--;
+    //         backtrack2(nums, temp, res, freq);
+    //         freq[nums[i]]++;
+    //         temp.pop_back();
+    //     }
+    // }
 };
