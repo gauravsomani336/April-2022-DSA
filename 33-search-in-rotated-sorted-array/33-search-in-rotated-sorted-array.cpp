97a592f9f5abfe5b80b165e91acb2d2e8e56f164
Gaurav somani <40043226+gauravsomani336@users.noreply.github.com>
2022-05-14 11:03:58 +0530

    Time: 3 ms (85.24%), Space: 11 MB (75.84%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,46 +1,74 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int s=0;
-        int e=nums.size()-1;
+        int l=0;
+        int n= nums.size();
+        int r= n-1;
         
-        int mid=s+(e-s)/2;
+      //  int mid= l+(r-l)/2;
         
-        while(s<=e)
-        {
-            if(target==nums[mid])
-            {
+//         while(l<=r)
+            
+//         {
+            
+//             int mid= (l+r)/2;
+//             if(nums[mid] == target)
+//             {
+//                 return mid;
+//             }
+            
+//             if(nums[mid] > nums[r])
+//             {
+//                  if (target < nums[mid] && target >= nums[l])
+//                 {
+//                     r=mid-1;
+//                 }
+//                 else
+//                 {
+//                     l=mid+1;
+//                 }
+//             }
+            
+//             else if(nums[mid]< nums[l])
+//             {
+//                  if (target > nums[mid] && target <= nums[r])
+//                 {
+//                     l=mid+1;
+//                 }
+//                 else 
+//                 {
+//                     r=mid-1;
+//                 }
+//             }
+            
+           
+//         }
+        
+         while (l <= r) {
+            int mid = (l+r) / 2;
+            if (target == nums[mid])
                 return mid;
+            // there exists rotation; the middle element is in the left part of the array
+            if (nums[mid] > nums[r]) {
+                if (target < nums[mid] && target >= nums[l])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
             }
-            //left sorted portion
-            if(nums[s]<= nums[mid])
-            {
-                if((target> nums[mid]) || target< nums[s])
-                {
-                   s=mid+1;
-                }
-                else 
-                {
-                    e=mid-1;
-                }
+            // there exists rotation; the middle element is in the right part of the array
+            else  {
+                if (target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
-            //right sorted portion
-            else
-            {
-                if((target < nums[mid] ) || target > nums[e])
-                   {
-                       e=mid-1;
-                   }
-                   else
-                   {
-                       s=mid+1;
-                   }
-            }
-            
-            mid=s+(e-s)/2;
-            
-            
-        }
+            //  else {
+            //     if (target < nums[mid])
+            //         r = mid - 1;
+            //     else
+            //         l = mid + 1;
+            // }
+         }
         
         return -1;
     }
