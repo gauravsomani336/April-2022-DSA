923dce93473216a1c9741f852d985070cb605c54
Gaurav somani <40043226+gauravsomani336@users.noreply.github.com>
2022-05-21 00:07:26 +0530

    Time: 141 ms (76.27%), Space: 47.1 MB (76.98%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -9,78 +9,79 @@
 //  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 //  * };
 //  */
-// class Solution {
-// public:
-//     void Inorder(vector<TreeNode*>&ans, TreeNode * root)
-//     {
-//         if(root==NULL)
-//         {
-//             return ;
-//         }
-        
-//          Inorder(ans, root->left);
-//         ans.push_back(root);
-//         Inorder(ans,root->right);
-        
-//     }
-//     TreeNode* balanceBST(TreeNode* root) {
-//         if(root==NULL)
-//             return NULL;
-//         vector<TreeNode*>ans;
-        
-//         Inorder(ans, root);
-       
-//         TreeNode * temp= solve(ans, 0, ans.size()-1);
-//         return temp;
-//     }
-    
-//     TreeNode * solve(vector<TreeNode*>ans, int start, int end)
-//     {
-//         if(start> end)
-//         {
-//             return NULL;
-//         }
-        
-//         int mid=(start+end)/2;
-        
-//         TreeNode * newNode= (ans[mid]);
-        
-//         newNode->left= solve(ans, start, mid-1);
-//         newNode->right= solve(ans, mid+1, end);
-        
-//         return newNode;
-        
-//     }
-// };
-
 class Solution {
 public:
-    vector<TreeNode*> inorder;
-    void findInorder(TreeNode* root)
+    vector<TreeNode*>ans;
+    void Inorder( TreeNode * root)
     {
         if(root==NULL)
-            return;
-        findInorder(root->left);
-        inorder.push_back(root);
-        findInorder(root->right);
+        {
+            return ;
+        }
+        
+         Inorder( root->left);
+        ans.push_back(root);
+        Inorder(root->right);
+        
     }
-    TreeNode* buildTree(int start,int end)
-    {
-        if(start>end)
+    TreeNode* balanceBST(TreeNode* root) {
+        if(root==NULL)
             return NULL;
+       // vector<TreeNode*>ans;
+        
+        Inorder( root);
+       
+        TreeNode * temp= solve( 0, ans.size()-1);
+        return temp;
+    }
+    
+    TreeNode * solve( int start, int end)
+    {
+        if(start> end)
+        {
+            return NULL;
+        }
+        
         int mid=(start+end)/2;
-        TreeNode* root=inorder[mid];
-        root->left=buildTree(start,mid-1);
-        root->right=buildTree(mid+1,end);
-        return root;
-    }
-    TreeNode* balanceBST(TreeNode* root) 
-    {
-        if(root==NULL)
-            return NULL;
-        findInorder(root);
-        int n=inorder.size();
-        root=buildTree(0,n-1);
-        return root;
+        
+        TreeNode * newNode= (ans[mid]);
+        
+        newNode->left= solve( start, mid-1);
+        newNode->right= solve( mid+1, end);
+        
+        return newNode;
+        
     }
 };
+
+// class Solution {
+// public:
+//     vector<TreeNode*> inorder;
+//     void findInorder(TreeNode* root)
+//     {
+//         if(root==NULL)
+//             return;
+//         findInorder(root->left);
+//         inorder.push_back(root);
+//         findInorder(root->right);
+//     }
+//     TreeNode* buildTree(int start,int end)
+//     {
+//         if(start>end)
+//             return NULL;
+//         int mid=(start+end)/2;
+//         TreeNode* root=inorder[mid];
+//         root->left=buildTree(start,mid-1);
+//         root->right=buildTree(mid+1,end);
+//         return root;
+//     }
+//     TreeNode* balanceBST(TreeNode* root) 
+//     {
+//         if(root==NULL)
+//             return NULL;
+//         findInorder(root);
+//         int n=inorder.size();
+//         root=buildTree(0,n-1);
+//         return root;
+//     }
+// };
